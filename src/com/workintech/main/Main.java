a0bcafa667676647bc3625ffaee02dcf56fd2615
AytacSahin <haytacsahin@gmail.com>
2023-08-01 14:09:22 +0300

    before tests & all classes added

--- src/com/workintech/main/Main.java
+++ src/com/workintech/main/Main.java
@@ -1,7 +1,10 @@
 package com.workintech.main;
 
+import com.workintech.enums.LampType;
+import com.workintech.enums.PaintColor;
+import com.workintech.model.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
-}
+ }
